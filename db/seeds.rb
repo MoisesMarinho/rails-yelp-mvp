36ac729d3efed9339ba4e7ac59a579a94aebe0a5
Moises Marinho <moisesmarinhosilva87@gmail.com>
2022-02-24 11:57:09 -0300

    feito rota do review pra create e new

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+Review.destroy_all
+
+puts 'Creating restaurants...'
+
+50.times do
+  restaurant = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  5.times do
+    Review.create!(
+      rating: rand(0..5),
+      content: Faker::Restaurant.review,
+      restaurant_id: restaurant.id
+    )
+  end
+end
+
+puts 'done!'
